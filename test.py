0208c907b8d04cbea1deebe95652cea22455161e
Mishanin Max <mmishanin@technoserv.com>
2016-01-11 18:27:29 +0300

    add some rus

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
 kernel._brain.setBotName("Melody")
-
+print(kernel._brain._botName)
 # Press CTRL-C to break this loop
 while True:
-    print(kernel._brain._botName)
+
     message = raw_input("Enter your message >> ")
     if message == "quit":
         print ("Bye")
